6f9bf1a848486d23dccbd318d6d12a4247ad6cd3
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-10-05 11:28:12 +0300

    add lesson_3

---
+++ semesters/3/parallel/003/lesson3/size_rank.cpp
@@ -0,0 +1,30 @@
+#include <mpi.h>
+
+#include <iostream>
+
+// int MPI_Comm_size (MPI_Comm comm, int *size); // кол-во процессов в группе
+// int MPI_Comm_rank (MPI_Comm comm, int *rank); // номера процессов в группе
+
+void AbortTest(int state) {
+  if (state != MPI_SUCCESS) MPI_Abort(MPI_COMM_WORLD, state);
+}
+
+int main(int argc, char *argv[]) {
+  int st;
+  st = MPI_Init(&argc, &argv);
+  AbortTest(st);
+
+  int size = 0;
+  st = MPI_Comm_size(MPI_COMM_WORLD, &size);
+  AbortTest(st);
+
+  int rank = 0;
+  st = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  AbortTest(st);
+
+  std::cout << "Hello World! "
+            << "We are processes, how many: " << size << "; our rank: " << rank
+            << std::endl;
+
+  MPI_Finalize();
+}
