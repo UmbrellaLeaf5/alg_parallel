a5777cedaca080155f668b908b9023f1a55443b2
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-10-12 10:46:13 +0300

    update structure

---
+++ semesters/3/parallel/lesson_4/send/send.cpp
@@ -0,0 +1,47 @@
+#include <mpi.h>
+
+#include <iostream>
+
+void AbortTest(int state) {
+  if (state != MPI_SUCCESS) MPI_Abort(MPI_COMM_WORLD, state);
+}
+
+int main(int argc, char *argv[]) {
+  int st;
+  st = MPI_Init(&argc, &argv);
+  AbortTest(st);
+
+  MPI_Status status;
+  int send_data = 17;
+  int recv_data;
+
+  int rank;
+  st = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  AbortTest(st);
+
+  int my_tag = 35817;
+  int dest_proc_rank = 1;
+  int from_proc_rank = 0;
+
+  if (rank == from_proc_rank) {
+    st = MPI_Send(&send_data, 1, MPI_INT, dest_proc_rank, my_tag,
+                  MPI_COMM_WORLD);
+    AbortTest(st);
+
+    std::cout << "Sended  : " << send_data
+              << "; from rank: " << rank /*       */
+              << "; with tag: " << my_tag << std::endl;
+  }
+
+  if (rank == dest_proc_rank) {
+    st = MPI_Recv(&recv_data, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
+                  MPI_COMM_WORLD, &status);
+    AbortTest(st);
+
+    std::cout << "Received: " << recv_data
+              << ";    source: " << status.MPI_SOURCE
+              << "; with tag: " << status.MPI_TAG << std::endl;
+  }
+
+  MPI_Finalize();
+}
---
+++ semesters/3/parallel/lesson_4/task_1/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#PBS -l walltime=00:01:00,nodes=3:ppn=2
+#PBS -N UL_lesson4
+#PBS -q batch
+
+cd $PBS_O_WORKDIR
+mpirun --hostfile $PBS_NODEFILE -np 6 ./a.out
